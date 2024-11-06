ea9646d01fc7c2335457aed6be20c90fc31d8437
anerhernandez <anerhernandez@gmail.com>
2024-11-06 15:55:50 +0000

    Se hicieron cambios en index, se añadio query selector y se eliminó un archivo

--- SerieTelevision/script.js
+++ SerieTelevision/script.js
@@ -1,7 +1,17 @@
-let pregunas = document.getElementById("Preguntas");
+let respuestas = ["Heisenberg" , "Fring", "Los Pollos Hermanos", "Pinkman", "Azul" , "Salamanca" , "Metilamina" , "Walter" , "Medicacion" , "McGill"]
 
-pregunas.addEventListener("submit", () => {
-  pregunas.get()
-  
-    // handle submit
-  });
+let botones = document.querySelectorAll(".desmarcar")
+botones.forEach(element => {
+  element.addEventListener('click', prevenir)
+});
+
+
+let BotonEnviar = document.getElementById("enviar");
+let formularios = document.querySelectorAll(".formularios")
+
+
+function prevenir(e){
+  e.preventDefault()
+  let formularioPadre = this.parentNode;
+  formularioPadre.reset();
+}
