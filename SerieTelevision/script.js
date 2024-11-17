76908fe0973f605e0168690ec52bcf25026a78b7
anerhernandez <116444676+anerhernandez@users.noreply.github.com>
2024-11-17 16:21:01 +0000

    Mod script

--- SerieTelevision/script.js
+++ SerieTelevision/script.js
@@ -1,31 +1,50 @@
-let respuestas = ["Heisenberg" , "Fring", "Los Pollos Hermanos", "Pinkman", "Azul" , "Salamanca" , "Metilamina" , "Walter" , "Medicacion" , "McGill"]
+let respuestas = [
+  "Heisenberg",
+  "Fring",
+  "Los Pollos Hermanos",
+  "Pinkman",
+  "Azul",
+  "Salamanca",
+  "Metilamina",
+  "Walter",
+  "Medicacion",
+  "McGill",
+];
 
-let botones = document.querySelectorAll(".desmarcar")
-botones.forEach(element => {
-  element.addEventListener('click', prevenir)
+let botones = document.querySelectorAll(".desmarcar");
+botones.forEach((element) => {
+  element.addEventListener("click", prevenir);
 });
 
-
 let BotonEnviar = document.getElementById("enviar");
-let formularios = document.querySelectorAll(".formularios")
+let formularios = document.querySelectorAll(".formularios");
 
-
-function prevenir(e){
-  e.preventDefault()
+function prevenir(e) {
+  e.preventDefault();
   let formularioPadre = this.parentNode;
   formularioPadre.reset();
 }
 
+let enviar = document
+  .getElementById("enviar")
+  .addEventListener("click", checarcorrectos);
 
-let enviar = document.getElementById("enviar").addEventListener('click', checarcorrectos);
-
-function checarcorrectos(){
-  let total_preguntas = [ "Pregunta 1", "Pregunta 2", "Pregunta 3", "Pregunta 4", "Pregunta 5",
-     "Pregunta 6", "Pregunta 7", "Pregunta 8", "Pregunta 9", "Pregunta 10", 
-    ]
-  let ya_chequeados = []
+function checarcorrectos() {
+  let total_preguntas = [
+    "Pregunta 1",
+    "Pregunta 2",
+    "Pregunta 3",
+    "Pregunta 4",
+    "Pregunta 5",
+    "Pregunta 6",
+    "Pregunta 7",
+    "Pregunta 8",
+    "Pregunta 9",
+    "Pregunta 10",
+  ];
+  let ya_chequeados = [];
   let chequeados = document.querySelectorAll("input[type=radio]:checked");
-  chequeados.forEach(element =>{
+  chequeados.forEach((element) => {
     if (respuestas.indexOf(element.value) > -1) {
       // let span = document.createElement("span")
       // span.setAttribute("id", "bien")
@@ -37,27 +56,41 @@ function checarcorrectos(){
       // span.textContent = " ❌"
       // element.parentNode.insertBefore(span, element.nextSibling.nextSibling.nextSibling)
     }
-    ya_chequeados.push(element.name)
+    ya_chequeados.push(element.name);
   });
   for (let i = 0; i < ya_chequeados.length; i++) {
     if (total_preguntas.indexOf(ya_chequeados[i]) != -1) {
-      total_preguntas.splice(total_preguntas.indexOf(ya_chequeados[i]), 1)
+      total_preguntas.splice(total_preguntas.indexOf(ya_chequeados[i]), 1);
     }
   }
-  
+
   if (chequeados.length < total_preguntas.length) {
-    total_preguntas.forEach(element => {
-      let nombre = document.getElementsByName(element)
-      let span = document.createElement("span")
-      span.setAttribute("id", "vacio")
+    total_preguntas.forEach((element) => {
+      let nombre = document.getElementsByName(element);
+      let span = document.createElement("span");
+      span.setAttribute("id", "vacio");
       span.textContent = "Tiene que rellenar este campo";
-      nombre[0].parentElement.prepend(span)
+      nombre[0].parentElement.prepend(span);
     });
-  } else {
-    document.querySelectorAll("span[id=vacio]").forEach(element => {
-      console.console.log(element);
-      element.textContent = "";
+    chequeados.forEach((element) => {
+      let span = element.parentNode.querySelector("span[id=vacio]")
+      if (span != null) {
+        element.parentNode.querySelector("span[id=vacio]").textContent = "";
+      }
+    });
+  }else{
+    chequeados.forEach((element) => {
+      if (respuestas.indexOf(element.value) > -1) {
+        let span = document.createElement("span")
+        span.setAttribute("id", "bien")
+        span.textContent = " ✅"
+        element.parentNode.insertBefore(span, element.nextSibling.nextSibling.nextSibling)
+      } else {
+        let span = document.createElement("span")
+        span.setAttribute("id", "mal")
+        span.textContent = " ❌"
+        element.parentNode.insertBefore(span, element.nextSibling.nextSibling.nextSibling)
+      }
     });
   }
-  
 }
