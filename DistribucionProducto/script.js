18e669e64757a13e613bf649415f7d6899125be4
anerhernandez <anerhernandezl@gmail.com>
2024-11-29 19:51:59 +0000

    Need to check regex

--- DistribucionProducto/script.js
+++ DistribucionProducto/script.js
@@ -16,5 +16,32 @@ document.getElementById("enviar").addEventListener("click", function (event) {
     console.log(cuenta.value)
 
 
+    let fecha_chk = 0;
+    let cocinero_chk = 0;
+    let destinatario_chk = 0;
+    let gramos_chk = 0;
+    let composicion_chk = 0;
+    let cuenta_chk = 0;
+
+    function isValidDateFormat(dateString) {
+        const regex = /^(\d{2})-(\d{2})-(\d{5})$/;
+        return regex.test(dateString);
+    }
+    //regex fecha_chk
+    if (!isNaN(fecha.getTime())) {
+        
+    } else {
+        
+    }
+    //regex cocinero_chk
+
+    //regex destinatario_chk
+
+    //regex gramos_chk
+
+    //regex composicion_chk
+
+    //regex cuenta_chk
+
 
 })
